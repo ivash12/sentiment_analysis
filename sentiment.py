b2481e53c492362f8a63014dad58401094a3e69e
ivan <iv.shmatko.dev@gmail.com>
2026-08-17 14:33:46 +0200

    train_test_split added

--- sentiment.py
+++ sentiment.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("data/tripadvisor_hotel_reviews.csv")
 print(df.shape)
@@ -12,3 +13,8 @@ def clean_text(text):
     return text
 
 df["clean_review"] = df["Review"].apply(clean_text)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    df["clean_review"], df["Rating"],
+    test_size=0.2, random_state=42, stratify=df["Rating"]
+)
