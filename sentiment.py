3e5211397db48eeff6f5ffaa563e4024d0fc2ff9
ivan <iv.shmatko.dev@gmail.com>
2026-09-03 22:53:58 +0200

    turning cleaned text into numeric vectors

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 df = pd.read_csv("data/tripadvisor_hotel_reviews.csv")
 print(df.shape)
@@ -18,3 +19,9 @@ X_train, X_test, y_train, y_test = train_test_split(
     df["clean_review"], df["Rating"],
     test_size=0.2, random_state=42, stratify=df["Rating"]
 )
+
+vectorizer = TfidfVectorizer(max_features=2000, stop_words="english")
+X_train_vec = vectorizer.fit_transform(X_train).toarray()
+X_test_vec = vectorizer.transform(X_test).toarray()
+
+print(X_train_vec.shape)
