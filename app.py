c8d45c0a84854fc3c5b1b801c01e9dd97f418bba
ivan <iv.shmatko.dev@gmail.com>
2026-09-11 21:52:19 +0200

    added simple ui in app.py. the program now accepts 2 languages (english and dutch)

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-from predict import predict_sentiment_en
-message = str(input("Enter your message here to get its grade: "))
-print(predict_sentiment_en(message))
+from predict import predict_sentiment_en, predict_sentiment_nl
+
+lan = input("choose your language. type 'en' for english and 'nl' for dutch: ")
+while True:
+    message = input("if you want to exit type: 'exit' \nenter your text: ")
+    if message == "exit":
+        print("thank you for using the model")
+        break
+    if lan == "en":
+        print(predict_sentiment_en(message))
+    elif lan == "nl":
+        print(predict_sentiment_nl(message))
+
+
 
