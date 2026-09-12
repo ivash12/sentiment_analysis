0f7b4b65c60b00a6c82356c05fb6af4d91f79f5c
ivan <iv.shmatko.dev@gmail.com>
2026-09-12 20:06:43 +0200

    streamlit added

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
+import streamlit as st
 from predict import predict_sentiment_en, predict_sentiment_nl
 
-lan = input("choose your language. type 'en' for english and 'nl' for dutch: ")
-while True:
-    message = input("if you want to exit type: 'exit' \nenter your text: ")
-    if message == "exit":
-        print("thank you for using the model")
-        break
-    if lan == "en":
-        print(predict_sentiment_en(message))
-    elif lan == "nl":
-        print(predict_sentiment_nl(message))
+st.title("Sentiment Analysis")
+
+language = st.selectbox("Choose a language", ["English", "Dutch"])
+text = st.text_area("Enter a review")
+
+if st.button("Predict"):
+    rating = predict_sentiment_en(text) if language == "English" else predict_sentiment_nl(text)
+    st.write(f"Predicted rating: {rating} star(s)")
 
 
 
